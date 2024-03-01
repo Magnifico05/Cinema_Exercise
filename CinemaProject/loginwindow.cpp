34622329c4efe2a7636490c5f000f52786770821
alikhattab <ali-khattab@aucegypt.edu>
2024-03-01 23:18:42 +0200

    login button finished, register remaining in the login window

--- CinemaProject/loginwindow.cpp
+++ CinemaProject/loginwindow.cpp
@@ -1,14 +1,48 @@
 #include "loginwindow.h"
+#include "Users.h"
 #include "ui_loginwindow.h"
+#include "welcomewindow.h"
+#include "registerwindow.h"
+
 
 LoginWindow::LoginWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::LoginWindow)
 {
     ui->setupUi(this);
+    ui->labelError->setVisible(false);
 }
 
 LoginWindow::~LoginWindow()
 {
     delete ui;
 }
+
+void LoginWindow::on_pushButtonLogin_clicked()
+{
+    bool Success = false;
+    for (int i = 0; i < usersCount; i++)
+    {
+        if ((ui->lineEditUsername->text() == usernames[i]) || (ui->lineEditPassword->text() == passwords[i]))
+            Success = true;
+    }
+
+
+    if (Success)
+    {
+    hide();
+    WelcomeWindow* welcome = new WelcomeWindow(this);
+    welcome->show();
+    }
+    else
+    {
+        ui->labelError->setVisible(true);
+    }
+}
+
+
+void LoginWindow::on_pushButtonRegister_clicked()
+{
+
+}
+
