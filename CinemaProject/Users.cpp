a9a207824c186376749ae7a8cf5210b6b387bdfe
alikhattab <ali-khattab@aucegypt.edu>
2024-03-01 22:55:14 +0200

    Finished the Global Arrays part

---
+++ CinemaProject/Users.cpp
@@ -0,0 +1,9 @@
+#include "Users.h"
+
+Qstring usernames[100] = {"admin", "test", "user1", "user2"};
+Qstring passwords[100] = {"admin", "1234", "1101", "1102"};
+int ages[100] = {20,21,22,23};
+int usersCount = 4;
+
+
+
